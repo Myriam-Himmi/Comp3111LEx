ba4ce677e3bdbb0350212675de9f871333013931
Myriam-Himmi <myriam.himmi.03@gmail.com>
2023-09-08 18:30:45 +0800

    Add 3rd print line

--- src/main/java/Lab1/myLibrary.java
+++ src/main/java/Lab1/myLibrary.java
@@ -4,7 +4,8 @@ public class myLibrary {
     public static int Power(int Base, int Exponent) {
         if (Exponent == 1)
             return Base * Exponent;
-        else return Base * Power(Base, Exponent - 1);
+        else
+            return Base * Power(Base, Exponent - 1);
     }
     public static int factorial(int n) {
         if(n == 1)
